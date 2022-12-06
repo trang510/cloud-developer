85185dc24b6bae0538ce059b3bb4b6356e0d8bc9
trangnd <trangnd@vng.com.vn>
2022-12-06 21:02:51 +0700

    implement getTodos

--- course-04/project/c4-final-project-starter-code/backend/src/helpers/todos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/helpers/todos.ts
@@ -1,5 +1,5 @@
-import { TodosAccess } from './todosAcess'
-import { AttachmentUtils } from './attachmentUtils';
+import { TodoAccess } from './todosAcess'
+// import { AttachmentUtils } from './attachmentUtils';
 import { TodoItem } from '../models/TodoItem'
 import { CreateTodoRequest } from '../requests/CreateTodoRequest'
 import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
@@ -8,3 +8,10 @@ import * as uuid from 'uuid'
 import * as createError from 'http-errors'
 
 // TODO: Implement businessLogic
+const logger = createLogger('todos')
+const todoAccess = new TodoAccess()
+
+export async function getTodosForUser(userId: string): Promise<TodoItem[]> {
+    logger.info(`Get all todos for user ${userId}`)
+    return await todoAccess.getAllTodos(userId)
+}
--- course-04/project/c4-final-project-starter-code/backend/src/helpers/todosAcess.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/helpers/todosAcess.ts
@@ -10,3 +10,21 @@ const XAWS = AWSXRay.captureAWS(AWS)
 const logger = createLogger('TodosAccess')
 
 // TODO: Implement the dataLayer logic
+export class TodoAccess {
+    constructor(
+        private readonly docClient: DocumentClient = new XAWS.DynamoDB.DocumentClient(),
+        private readonly todosTable = process.env.TODOS_TABLE,
+        private readonly todosByUserIndex = process.env.TODOS_BY_USER_INDEX
+      ) {}
+    
+
+    async getAllTodos(userId: string): Promise<TodoItem[]> {
+        const result = await this.docClient.query({
+            TableName: this.todosTable,
+            KeyConditionExpression: 'userId = :userId',
+            ExpressionAttributeValues: {':userId': userId},
+        }).promise()
+        
+        return result.Items as TodoItem[]
+      }      
+}
